ef55d3b0c13b22b347162a96b6daf2a73c8f82c1
shrimp-f <fujitadynamics@gmail.com>
2019-07-08 18:47:40 +0900

    first

---
+++ src/imu2tf_converter.cpp
@@ -0,0 +1,40 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <turtlesim/Pose.h>
+#include <sensor_msgs/Imu.h>
+
+std::string turtle_name;
+
+
+
+void poseCallback(const sensor_msgs::Imu& msg){
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+    tf::Quaternion q;
+    double g = 10.0;
+//    double sum = msg.linear_acceleration.x + msg.linear_acceleration.y + msg.linear_acceleration.z;
+    double r,p;
+    r = atan(msg.linear_acceleration.y/msg.linear_acceleration.z);
+    p = atan(-msg.linear_acceleration.x/(sqrt(pow(msg.linear_acceleration.y,2)+pow(msg.linear_acceleration.z,2))));
+//    q.setRPY(msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z);
+    q.setRPY(r, p, 0);
+    transform.setRotation(q);
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "camera_link"));
+}
+
+
+
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "my_tf_broadcaster");
+/*
+    if (argc != 2){ROS_ERROR("need turtle name as argument"); return -1;};
+    turtle_name = argv[1];
+*/
+    ros::NodeHandle node;
+    ros::Subscriber sub = node.subscribe("/camera/accel/sample", 10, &poseCallback);
+
+    ros::spin();
+    return 0;
+};
